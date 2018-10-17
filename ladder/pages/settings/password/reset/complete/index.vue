a2d31d424451a760ccbf310ce31ab85707b04d73
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2018-10-18 01:39:16 +0900

    added received token page

---
+++ ladder/pages/settings/password/reset/complete/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-layout align-center justify-start column
+            class="layout-complete">
+  </v-layout>
+</template>
+
+<script>
+  export default {
+    name: "password-complete",
+    layout: 'engineer',
+    scrollToTop: true,
+    transitions: {
+      name: 'page',
+      mode: 'out-in'
+    },
+    data: () => ({}),
+    head() {
+      return {
+        title: 'パスワードリセット'
+      }
+    },
+    methods: {},
+  }
+</script>
+
+<style scoped lang="sass"></style>
