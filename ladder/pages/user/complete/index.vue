97dae7e5c36026bcf4e554c3fed837f5af07362b
tetty0217 <tettaro_01291978@ezweb.ne.jp>
2018-10-17 16:41:37 +0900

    added user complete page

---
+++ ladder/pages/user/complete/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-layout align-center justify-start column
+            class="layout-user-complete">
+  </v-layout>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: "user-complete",
+    layout: 'engineer',
+    scrollToTop: true,
+    transitions: {
+      name: 'page',
+      mode: 'out-in'
+    },
+    data: () => ({}),
+    head() {
+      return {
+        title: 'ユーザ-登録完了'
+      }
+    },
+    components: {},
+    methods: {},
+    computed: {
+    },
+  }
+</script>
+
+<style scoped lang="sass">
+
+</style>
